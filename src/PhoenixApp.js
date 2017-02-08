164d1890ef9d48893f8b9d313cc692f5fb8b1935
Sukhrob Rakhimov <suhrob@gmail.com>
2017-02-08 17:21:17 +0300

    adding shouldComponentUpdate for the app

--- src/PhoenixApp.js
+++ src/PhoenixApp.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import Layers from './components/layers/Layers';
-import Chart from './components/Chart/Chart';
+import Chart from './components/chart/Chart';
 import style from './phonix.sass';
+const _ = require('lodash');
 
 class PhoenixApp extends Component {
 
   constructor(props) {
     super(props);
     const isins = [];
-    this.state = { isins };
+    this.state = {isins};
 
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    if(_.isEqual(nextState, this.state)) {
+      return false;
+    }
+    return true;
+  }
+
   handleFilterChange(isins) {
-    console.log(isins);
     this.setState({isins: isins});
   }
 
@@ -22,8 +29,8 @@ class PhoenixApp extends Component {
     return (
       <div>
         {this.props.reportName}
-        <Layers filteredDataHandler={this.handleFilterChange.bind(this)} />
-        <Chart isins={this.state.isins} />
+        <Layers key='reportLayers' filteredDataHandler={this.handleFilterChange.bind(this)} />
+        <Chart key='scatterPlotChart' isins={this.state.isins} />
       </div>
     );
   }
