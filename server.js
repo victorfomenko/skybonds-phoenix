23dfaed2c7026c6bb3a1109085548535d53a6472
Gleb Kondratenko <gkondratenko@gmail.com>
2017-02-08 15:06:28 +0300

    feature: integrated charts

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var path = require('path');
+var url = require('url');
+var fs = require('fs');
 var webpack = require('webpack');
 var express = require('express');
+var compression = require('compression');
 var devMiddleware = require('webpack-dev-middleware');
 var hotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
@@ -13,7 +16,24 @@ app.use(devMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
+app.use(compression());
 app.use(hotMiddleware(compiler));
+
+// app.get('*', function(req, res, next) {
+//   var urlPieces = url.parse(req.url).pathname.split(/[/\\]/);
+//   var p = path.join(__dirname, req.url);
+//   if (urlPieces[urlPieces.length - 1] != '' && fs.existsSync(p)) {
+//     res.sendFile(path.join(__dirname, 'vendors/skybonds.components.js'));
+//     // res.sendFile(p);
+//   } else {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+//   }
+//   // return next();
+// });
+
+app.get('**/skybonds.components.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'vendors/skybonds.components.js'));
+});
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
