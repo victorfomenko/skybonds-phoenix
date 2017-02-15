f5cba0878390cba182e9ed13f69f6a942a66142c
Gleb Kondratenko <gkondratenko@gmail.com>
2017-02-15 14:34:35 +0300

    refactor: unused import

--- src/data/clients/UserApi.js
+++ src/data/clients/UserApi.js
@@ -1,24 +1,22 @@
-import { requestProvider, localStorageProvider } from '../helpers';
-
+import { requestProvider } from '../helpers';
 
 const API_V1 = '/api/v1/user/';
-
 
 export const auth = (email, pwd) => {
   return requestProvider.post({
     url: API_V1 + 'auth',
     body: { email, pwd }
   })
-}
+};
 
 export const current = () => {
   return requestProvider.get({
     url: API_V1 + 'current'
   })
-}
+};
 
 export const logout = () => {
   return requestProvider.post({
     url: API_V1 + 'logout'
   })
-}
+};
