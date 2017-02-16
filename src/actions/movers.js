a85cbc1e2aca98e767346ac8924b219aabc32061
Sukhrob Rakhimov <suhrob@gmail.com>
2017-02-16 03:24:18 +0300

    small fix: changed types to actionTypes

--- src/actions/movers.js
+++ src/actions/movers.js
@@ -3,13 +3,13 @@ import * as Movers from '../data/providers/Movers'
 
 
 export const loadMovers = ({isins, startDate, endDate, selectedPeriod, selectedUnit}) => async (dispatch) => {
-  dispatch({ type: types.LOAD_MOVERS });
+  dispatch({ type: actionTypes.LOAD_MOVERS });
   try {
     const data = await Movers.loadMovers({isins, startDate, endDate, paramName: selectedUnit});
-    dispatch({ type: types.LOAD_MOVERS_SUCCESS, data, isins, selectedPeriod, selectedUnit });
+    dispatch({ type: actionTypes.LOAD_MOVERS_SUCCESS, data, isins, selectedPeriod, selectedUnit });
   }
   catch (error) {
-    dispatch({ type: types.LOAD_MOVERS_FAILURE, data: error });
+    dispatch({ type: actionTypes.LOAD_MOVERS_FAILURE, data: error });
   }
 }
 
