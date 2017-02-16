4689ee5dfb43e405db5f58c4bda13dfd34b9bf19
Gleb Kondratenko <gkondratenko@gmail.com>
2017-02-16 12:36:01 +0300

    fix: lint

--- src/pages/Bond/BondGeneral/BondGeneral.js
+++ src/pages/Bond/BondGeneral/BondGeneral.js
@@ -58,7 +58,7 @@ class BondGeneral extends Component {
   }
 
   parseDate(str) {
-    if(!/^(\d){8}$/.test(str)) return "invalid date";
+    if(!/^(\d){8}$/.test(str)) return 'invalid date';
     var y = str.substr(0,4),
       m = str.substr(4,2),
       d = str.substr(6,2);
