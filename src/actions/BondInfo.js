203be940083a03b1c42c4f9f9e44b4d72d997da9
LordWalmar <LordWalmar@ya.ru>
2017-02-24 19:50:47 +0300

    feature: added time-series chart

--- src/actions/BondInfo.js
+++ src/actions/BondInfo.js
@@ -7,35 +7,17 @@ export const openBondInfo = (isin, date) => async (dispatch, getState) => {
     let infoAttrs = ['isin', 'standardName', 'maturityDate', 'issuerId', 'issuer', 'sector', 'outlook', 'ratingGroup'];
 
     let info = await DataProvider.getBondsInfo([isin], infoAttrs);
-    console.log('DataProvider.getBondsInfo([isin], infoAttrs);', DataProvider.getBondsInfo([isin], infoAttrs));
-    //let daily = await DataProvider.getBondsDaily([isin], date, dailyAttrs);
-    //let putDates = await DataProvider.getBondsSchedulePut(isin);
+    let daily = await DataProvider.getBondsDaily([isin], date, dailyAttrs);
+    let putDates = await DataProvider.getBondsSchedulePut(isin);
 
     dispatch({
       type: actionTypes.OPEN_BOND_INFO,
-      bond: {
-        'isin': isin,
-        'info': info.data
-        //'daily': daily.data,
-        //'putDates': putDates.data
-      }
+      isin: isin,
+      date: date,
+      info: info[0].data,
+      daily: daily[0].data,
+      putDates: putDates.data
     });
-
-    //Promise.all([
-    //  DataProvider.getBondsInfo([isin], infoAttrs),
-    //  DataProvider.getBondsDaily([isin], date, dailyAttrs),
-    //  DataProvider.getBondsSchedulePut(isin)
-    //]).then((response) => {
-    //  dispatch({
-    //    type: actionTypes.OPEN_BOND_INFO,
-    //    bond: {
-    //      'isin': isin,
-    //      'info': response[0][0].data,
-    //      'daily': response[1][0].data,
-    //      'putDates': response[2].data
-    //    }
-    //  });
-    //})
   }
   catch (response) {
     dispatch({
--- src/components/BondInfo/index.js
+++ src/components/BondInfo/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { getColor } from '../../helpers/BondRating';
 import { connect } from 'react-redux';
-import * as Data from '../../data/providers/Data';
-import DateFormatter from '../../helpers/formatters/DateFormatter';
+import BondInfoHeader from '../BondInfoHeader';
+import BondInfoTimeSeries from '../BondInfoTimeSeries';
+import DateDayCaster from '../../data/casters/DateDayCaster';
 import { getLabel } from '../../helpers/BondOutlook';
 import { closeBondInfo } from '../../actions';
 import { Icon, GLYPHS } from '../../components/Icon';
@@ -11,61 +12,27 @@ import style from './style.sass';
 class BondInfo extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      'isin': null,
-      'loaded': false,
-      'info': null,
-      'daily': null
-    };
-
+    this.bond = null;
     this.onClickClose = this.onClickClose.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    this.date = new Date(this.props.date);
-    console.log('this.props', this.props);
-    let isin = nextProps.bondInfo.isin;
-
-    if (isin != null ) {
-      this.initBond(isin, this.date);
-    };
-
-  }
-
-  initBond(isin = null, date = null) {
-
-    let dailyAttrs = ['coupon'];
-    let infoAttrs = ['isin', 'standardName', 'maturityDate', 'issuerId', 'issuer', 'sector', 'outlook', 'ratingGroup'];
-    if(isin != null && date != null ) {
-      Promise.all([
-        Data.getBondsInfo([isin], infoAttrs),
-        Data.getBondsDaily([isin], date, dailyAttrs),
-        Data.getBondsSchedulePut(isin)
-      ]).then((response) => {
-        this.setState({
-          'loaded': true,
-          'bond': {
-            'info': response[0][0].data,
-            'daily': response[1][0].data,
-            'isin': isin,
-            'putDate': this.getPutCallDate(response[2].data)
-          }
-
-        });
-
-      });
+    this.bond = nextProps.bondInfo;
+    let putDate = this.getPutCallDate(nextProps.bondInfo.putDates);
+    if (putDate != null){
+      putDate = DateDayCaster.cast(putDate)
     }
+    this.bond.putDate = putDate
   }
 
   getPutCallDate(dates) {
-    if (dates.length == 0) {
+    if (dates == null || dates.length == 0) {
       return null
     }
 
     let date =  dates.find((function (_this) {
       return function (date) {
-        return _this.parseDate(date) > _this.date
+        return DateDayCaster.cast(date) > _this.bond.date
       };
     })(this));
 
@@ -75,103 +42,29 @@ class BondInfo extends Component {
     return date
   }
 
-  parseDate(str) {
-    if(!/^(\d){8}$/.test(str)) return 'invalid date';
-    var y = str.substr(0,4),
-      m = str.substr(4,2),
-      d = str.substr(6,2);
-
-    return new Date(y,(m-1),d);
-  }
-
   onClickClose() {
     this.props.closeBondInfo()
   }
 
-  toggleExclude() {
-
-  }
-
-  isExcludedIssuer() {
-    return true
-  }
-
-  toggleExcludeIssuer() {
-
-  }
-
-  addSetOfSimilarBonds() {
-
-  }
-
   render(){
-    const bond = this.state.bond;
-    if(this.state.loaded) {
+    if(this.bond && this.bond.isin ) {
       return (
-        <div className={style.reportAsideBondGeneral + ' ' + (this.props.bondInfo.show ? style.__active : '')}>
+        <div className={style.reportAsideBondGeneral + ' ' + (this.bond.show ? style.__active : '')}>
           <div className={style.reportAsideBond}>
             <span className={style.reportAsideBond_link + ' ' + style.reportAsideBond_close}
                   onClick={ () => this.onClickClose()}>
             <Icon glyph={GLYPHS.CLOSE}
                   width="30" height="30" />
             </span>
-            <div className={style.reportAsideBondHeader}>
-              <ul className={style.reportAsideBondHeader_rows}>
-                <li className={style.reportAsideBondHeader_line}>
-                  <a href={'/bond/' + bond.info.isin } target="_blank"
-                     className={style.reportAsideBond_link + ' ' + style.reportAsideBondHeader_title}>{bond.info.standardName}</a>
-                  {/*<input type="checkbox" className={style.reportAsideBondHeader_excluded + ' ' + style.__bond}
-                         onClick={this.toggleExclude()}/>
-                         */}
-                </li>
-                <li className={style.reportAsideBondHeader_line}>
-                  <ul className={style.reportAsideBondHeader_list}>
-                    <li className={style.reportAsideBondHeader_item}>
-                      <span>Maturity date </span>
-                      <span>{DateFormatter(this.parseDate(bond.info.maturityDate))}</span>
-                    </li>
-                    <li className={style.reportAsideBondHeader_item}>
-                      <span>Coupon </span>
-                      <span>{bond.daily.coupon}</span>
-                    </li>
-                    { bond.putDate ?
-                      <li className={style.reportAsideBondHeader_item}>
-                        <span>({DateFormatter(this.parseDate(bond.putDate))})</span>
-                      </li>
-                      : ''
-                    }
-                    <li className={style.reportAsideBondHeader_line}>
-                      <ul className={style.reportAsideBondHeader_list}>
-                        <li className={style.reportAsideBondHeader_item + ' ' + style.reportAsideBondHeader_item__issuer}>
-                          {/* !this.isExcludedIssuer() ?
-                            <input type="checkbox" className={style.reportAsideBondHeader_excluded + ' ' + style.__issuer}
-                                   onClick={this.toggleExcludeIssuer()} checked/>
-                            :
-                            <input type="checkbox" className={style.reportAsideBondHeader_excluded + ' ' + style.__issuer}
-                                   onClick={this.toggleExcludeIssuer()}/>
-                          */}
-                          <a href={'/issuer/' + bond.info.issuerId }
-                             className={style.reportAsideBond_link}>{bond.info.issuer}</a>
-                        </li>
-                        <li className={style.reportAsideBondHeader_item}>
-                          <a href="" className={style.reportAsideBond_link} onClick={this.addSetOfSimilarBonds()}></a>
-                          <span>{bond.info.sector}, </span>
-                          <span style={{color: getColor(bond.info.ratingGroup)}}>{bond.info.ratingGroup}</span>
-                        </li>
-                        <li className={style.reportAsideBondHeader_item + ' ' + style.reportAsideBondHeader_outlook}>
-                          <span>{ getLabel(bond.info.outlook) }</span>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li className={style.reportAsideBondHeader_line}>
-                  <ul className={style.reportAsideBondHeader_list}>
-                    <li className={style.reportAsideBondHeader_item}>{bond.isin}</li>
-                  </ul>
-                </li>
-              </ul>
+            <BondInfoHeader
+              bond={this.bond}
+            />
+            <div className={style.reportAsideBondContent}>
+              <div className={style.reportAsideBondContent_wrap}>
+                <BondInfoTimeSeries bond={this.bond} yAxis='yield' />
+              </div>
             </div>
+
           </div>
         </div>
       );
