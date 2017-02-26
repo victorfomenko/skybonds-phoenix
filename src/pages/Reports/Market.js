1d4e1a9377fcd6c26c3b5d00808427bc909df3a9
Gleb Kondratenko <gkondratenko@gmail.com>
2017-02-26 18:07:48 +0300

    feature: commom calendar

--- src/pages/Reports/Market.js
+++ src/pages/Reports/Market.js
@@ -8,11 +8,16 @@ import ScatterPlot from '../../components/ScatterPlot';
 import Movers from '../../components/Movers';
 import { Icon, GLYPHS } from '../../components/Icon';
 
+import DateFormatter from '../../helpers/formatters/DateFormatter';
 import { MARKET_REPORT_VIEW_MODES } from '../../data/constants';
 import { getSpaces } from '../../data/providers/Spaces';
 import { loadReports } from '../../actions';
 
 import styles from './styles.sass';
+
+const formatNativeInputDate = (date) => {
+  return DateFormatter(date, {regexp: '$3-$2-$1'})
+};
 
 class Market extends Component {
 
@@ -23,7 +28,7 @@ class Market extends Component {
       reportIsins: [],
       activeIsin: '',
       viewMode: MARKET_REPORT_VIEW_MODES.SCATTERPLOT,
-      date: '',
+      dateToday: this.props.summary.today,
       reportID: props.match.params.reportID
     };
     this.onViewModeChange = this.onViewModeChange.bind(this);
@@ -51,7 +56,14 @@ class Market extends Component {
   }
 
   onDateChange(e) {
-    this.setState(e.target.value)
+    let newDate = new Date(e.target.value);
+    if(isNaN(newDate.getTime()) ||
+      newDate.getTime() < this.props.summary.dataSince ||
+      newDate.getTime() > this.props.summary.today)
+    {
+      newDate = this.props.summary.today;
+    }
+    this.setState({ dateToday: newDate });
   }
 
   onViewModeChange(viewMode) {
@@ -66,8 +78,14 @@ class Market extends Component {
         { market ?
           <div className={styles.reportWrap}>
             <div className={styles.reportHeader}>
-              <Layers />
-              <div className={styles.reportDate}><input type="date" value={this.state.date} onChange={this.onDateChange.bind(this)}/></div>
+              <Layers dateToday={this.state.dateToday} />
+              <div className={styles.reportDate}>
+                <input type="date"
+                       value={formatNativeInputDate(this.state.dateToday)}
+                       min={formatNativeInputDate(this.props.summary.dataSince)}
+                       max={formatNativeInputDate(this.props.summary.today)}
+                       onChange={this.onDateChange.bind(this)}/>
+              </div>
               <div className={styles.reportViewMode}>
                 <ul className={styles.reportViewMode_list}>
                   <li className={styles.reportViewMode_item + (this.state.viewMode === MARKET_REPORT_VIEW_MODES.SCATTERPLOT ? ' ' + styles.__active : '')} onClick={()=>this.onViewModeChange(MARKET_REPORT_VIEW_MODES.SCATTERPLOT)}>
@@ -82,12 +100,14 @@ class Market extends Component {
                 <div className={styles.reportView_content}>
                   <ScatterPlot
                     isins={this.state.reportIsins}
+                    dateToday={this.state.dateToday}
                     activeIsin={this.state.activeIsin}
                     onActiveIsinChange={this.onActiveIsinChange.bind(this)} />
                 </div>
                 <div className={styles.reportView_aside}>
                   <Movers
                     isins={this.state.reportIsins}
+                    dateToday={this.state.dateToday}
                     onActiveIsinChange={this.onActiveIsinChange.bind(this)} />
                 </div>
               </div>
@@ -105,5 +125,5 @@ Market.propTypes = {
   loadReports: React.PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({ user: state.user, market: state.reports.market });
+const mapStateToProps = state => ({ user: state.user, summary: state.summary, market: state.reports.market });
 export default connect(mapStateToProps, { loadReports })(Market);
